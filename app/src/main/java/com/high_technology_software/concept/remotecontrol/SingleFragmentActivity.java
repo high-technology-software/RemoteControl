dfb3547a8e01616b0a0a237dfbedcab0b7b92dea
foxtrot-developer <albertmotosgonzalez@gmail.com>
2015-12-16 13:25:19 +0100

    XML Drawables and 9-patches

--- app/src/main/java/com/high_technology_software/concept/remotecontrol/SingleFragmentActivity.java
+++ app/src/main/java/com/high_technology_software/concept/remotecontrol/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.high_technology_software.concept.remotecontrol;
 
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,23 +10,18 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
 
     protected abstract Fragment createFragment();
 
-    protected int getLayoutResId() {
-        return R.layout.activity_fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutResId());
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
 
         if (fragment == null) {
             fragment = createFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
-                    .commit();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
         }
     }
 }
